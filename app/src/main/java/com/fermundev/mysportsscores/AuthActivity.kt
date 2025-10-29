17ee5278ce5d16c177c30d7b3b4a93ec73d336e2
Luis Felipe Ferrer Delgado <lfdelgado@stefanini.com>
2025-10-29 12:05:04 -0500

    Se crea la primer pantalla, la pantalla de autenticación.

--- app/src/main/java/com/fermundev/mysportsscores/AuthActivity.kt
+++ app/src/main/java/com/fermundev/mysportsscores/AuthActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.analytics.FirebaseAnalytics
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Analytics Events
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase exitosa!")
+        analytics.logEvent("InitScreen", bundle)
     }
 }
